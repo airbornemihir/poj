bb2918650aa498116e8f95b756be93bbbcc84b30
Mihir Mehta <mihir@cs.utexas.edu>
2022-01-13 00:40:37 -0800

    Work on a list reversal problem

---
+++ reverse-nodes-in-k-group/1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+      // The k part is not strictly necessary.
+      vector<ListNode*> pointers(k);
+      int i1 = 0; ListNode *cur = head;
+      while ((i1 < k) && (cur != nullptr)) {
+        pointers[i1] = cur->next;
+        cur = cur->next;
+        ++i1;
+      }
+      if (i1 < k) {
+        return head;
+      }
+      else {
+        for (int i2 = 0; i2 < k - 1; i2++) {
+          pointers[i2]->next = (i2 == 0) ? head : pointers[i2 - 1];
+        }
+        head->next = reverseKGroup(pointers[k - 1]->next, k);
+        return pointers[k - 2];
+      }
+    }
+};
+
+int main () {
+return 0;}
